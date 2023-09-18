fb107e2f4024f1d058afcf057ab5e53ec2867034
AgathaKarenne <142948129+AgathaKarenne@users.noreply.github.com>
2023-09-18 20:28:56 -0300

    Update exercicio3.c

--- exercicio3.c
+++ exercicio3.c
@@ -5,14 +5,20 @@ main(){
     /*Faça um programa que leia um número inteiro N
      e depois imprima os N primeiros números naturais ímpares (sem usar comando condicional). */
 
-     int numero, impar = 1, N;
+     int n, i = 0, impar = 1;
      printf("Digite um numero inteiro");
-     scanf("%d", &numero);
+     scanf("%d", &n);
 
-    
-     while (N <= 0){
-        printf("%d\n", impar);
-        impar += 2;
-        N --;
-     }
+
+    while (i < n) {
+    printf("%d\n", impar);
+    impar += 2;
+    i++;
+}
+
+   for (i = 1; n > 0; i += 2, n--) {
+        printf("%d\n", i);
+    }
+
+
 }
