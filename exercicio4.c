655acae19e040fdb5264e4d3d40f4b7649e87240
AgathaKarenne <142948129+AgathaKarenne@users.noreply.github.com>
2023-09-19 10:09:01 -0300

    Update exercicio4.c

--- exercicio4.c
+++ exercicio4.c
@@ -5,16 +5,19 @@ main(){
     /*Faça um programa que determine e mostre os cinco primeiros múltiplos de 3 considerando números maiores que 0.*/
 
     int n = 1, mult, resto;
-    int divisor = 3, contador = 0;
+    int divisor = 3, contador = 1;
 
     printf("Quais são os cinco primeiros multiplos de 3 maiores que 0:\n ");
-    scanf("%d\n", n);
 
-    do {
+   
+      while (contador <= 50){
+        if(divisor %3 == 0){
+            printf("\nmostre o divisor %d", divisor);
+            contador++;
+        }
+        divisor++;
+      }
 
-    printf("%d\n", contador);
-    contador = n * mult;
-}       while (contador < 5);
 
     
 }
