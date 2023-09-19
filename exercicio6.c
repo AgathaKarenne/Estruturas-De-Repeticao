c78ce1a1ab4263b527b7e7bea647945e5d61a5d6
AgathaKarenne <142948129+AgathaKarenne@users.noreply.github.com>
2023-09-19 11:07:18 -0300

    Add files via upload

---
+++ exercicio6.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+main(){
+
+    /*Elabore um algoritmo que permita ao usuário realizar a compra de frutas.
+     Assim, apresente a lista de frutas disponíveis, abaixo, com seus respectivos valores.
+     O algoritmo deve permitir ao usuário retomar o menu frutas quantas vezes quiser,
+      e solicitar a quantidade de frutas. Ao final, apresente o valor total da compra. 
+
+     1 => ABACAXI – 5,00 a unidade 2 => MAÇA – 1,00 a unidade 3 => PERA – 4,00 a unidade*/
+
+    int frutas, valor, quantidade, valorTotal;
+
+    switch(frutas){
+    case 1:
+        printf("Abacaxi :R$ 5,00 a unidade\n");
+        break;
+    case 2:
+        printf("Maçã :R$ 1,00 a unidade\n");
+        break;
+    case 3:
+        printf("Pera: R$ 4,00 a unidade\n");
+        break;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
