27c89c947b3b9aca961643157594a4cf3b150725
AgathaKarenne <142948129+AgathaKarenne@users.noreply.github.com>
2023-09-19 10:11:55 -0300

    Update exercicio5.c

--- exercicio5.c
+++ exercicio5.c
@@ -4,13 +4,17 @@ main(){
 
     /* Faça um programa que calcule e mostre a soma dos 50 primeiros números pares.*/
 
-    int contador, soma = 0;
+    int contador, soma = 0, pares = 0;
 
     printf("Os primeiros numeros pares de 50 são: \n");
     
     for (contador = 1; contador <= 50; contador++)
     {
-        /* code */
+        pares = pares + 2;
+        soma += pares;
+        
+
+    printf("\nMostre a soma %d %d %d", contador, pares,  soma);
     }
 }
     
