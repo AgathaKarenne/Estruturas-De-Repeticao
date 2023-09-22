b408bf404e575781aaa7063a431286c59561373a
AgathaKarenne <142948129+AgathaKarenne@users.noreply.github.com>
2023-09-22 17:34:33 -0300

    Update exercicio6.c

--- exercicio6.c
+++ exercicio6.c
@@ -8,28 +8,41 @@ main(){
       e solicitar a quantidade de frutas. Ao final, apresente o valor total da compra. 
      1 => ABACAXI – 5,00 a unidade 2 => MAÇA – 1,00 a unidade 3 => PERA – 4,00 a unidade*/
 
-    int menufrutas, valor, quantidade, valorTotal, continuar, total = 0.0;
-
-    switch(menufrutas){
-    case 1:
-        printf("Abacaxi :R$ 5,00 a unidade\n");
-        break;
-    case 2:
-        printf("Maçã :R$ 1,00 a unidade\n");
-        break;
-    case 3:
-        printf("Pera: R$ 4,00 a unidade\n");
-        break;
+    int valor, continuar, opção, valorTotal = 0.0;
+    
+    do{
+        printf("Menu de Frutas:\n");
+        printf("1. Abacaxi - R$5.00 por unidade\n");
+        printf("2. Maçã - R$1.00 por unidade\n");
+        printf("3. Pera - R$4.00 por unidade\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
     }
 
-    while (continuar == 0); {
-        printf("solicite a quantidade de frutas:");
-        if("%d", &quantidade);{
-                    total += quantidade * valorTotal;
-                    total += quantidade * valorTotal;
-        }
+    switch(opção){
+        case 01
+            printf("quantos abacaxis deseja comprar");
+            scanf("%d", &quantidade);
+            total += quantidade * 5.00;
+            break;
+         case 02
+            printf("quantas maças deseja comprar");
+            scanf("%d", &quantidade);
+            total += quantidade * 1.00;
+            break;
+         case 03
+            printf("quantas peras deseja comprar");
+            scanf("%d", &quantidade);
+            total += quantidade *4.00;
+            break;
+   
+   
         
     } printf("\nMostre o valor final da compra %d %d %d", valor, quantidade, valorTotal);
 
-}
+}while (opção != 1, 2, 3);
+    printf("Esta fruta não esta disponivel");
+    scanf("%d", &opção);
+    EXIT_SUCCESS
+
 
